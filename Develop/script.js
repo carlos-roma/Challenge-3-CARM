1e5ca43dac57b5d3040df0616ad90785417f3894
carlos-roma <adrianc.rm0@gmail.com>
2024-05-27 23:59:32 -0600

    Submitting challenge 3

---
+++ Develop/script.js
@@ -0,0 +1,74 @@
+// Assignment code here
+function initPassword() {
+  var usergreet = alert("Welcome to the password generator, click to continue");
+  var userInput = prompt("Do you want to create a password?");
+  if (userInput === "Yes" || userInput === "y" || userInput === "yes" || userInput === "YES" || userInput === "Y") {
+    var password = generatePassword();
+    return password;
+  } else if (userInput === "No" || userInput === "no" || userInput === "NO" || userInput === "n") {
+    alert("Thanks for using Password generator");
+  } else {
+    alert("Please enter a valid answer (Yes/No)");
+    return initPassword();
+  }
+}
+
+function generatePassword() {
+  var passLength = prompt("How long do you want the password to be? (8-128 characters)");
+  
+  // Validate password length input
+  if (isNaN(passLength)) {
+    alert("Please enter a valid number");
+    return generatePassword();
+  }
+  if (passLength < 8 || passLength > 128) {
+    alert("Password has to be between 8-128 characters long");
+    return generatePassword();
+  }
+
+  // Confirm character types to include
+  var includeUpper = confirm("Do you want to include upper case characters?(OK for yes, cancel for no)");
+  var includeLower = confirm("Do you want to include lower case characters? (OK for yes, cancel for no)");
+  var includeNumber = confirm("Do you want to include numbers? (OK for yes, cancel for no)");
+  var includeSpecial = confirm("Do you want to include special characters? (OK for yes, cancel for no)");
+  
+  // Validate that at least one character type is selected
+  if (!includeLower && !includeUpper && !includeNumber && !includeSpecial) {
+    alert("At least one type of character must be selected for the password.");
+    return generatePassword();
+  }
+
+  // Character to build password
+  var lowerC = 'abcdefghijklmnopqrstuvwxyz';
+  var upperC = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var numbers = '0123456789';
+  var specialC = '!@#$%^&*()-_+=[]{}|;:\'",.<>?/';
+
+  var allCharacters = '';
+  if (includeLower) allCharacters += lowerC;
+  if (includeUpper) allCharacters += upperC;
+  if (includeNumber) allCharacters += numbers;
+  if (includeSpecial) allCharacters += specialC;
+
+  //Logic to create the password
+  var passchar = '';
+  for (var i = 0; i < passLength; i++) {
+    var randomIndex = Math.floor(Math.random() * allCharacters.length);
+    passchar += allCharacters[randomIndex];
+  }
+
+  return passchar;
+}
+
+// connect #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = initPassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
+
+//event listener to generate button
+generateBtn.addEventListener("click", writePassword);
--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# Challenge-3-CARM
+# Password generator
+  ![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)
+  
+  ## Table of Contents
+  * [Description](#description)
+  * [Installation](#installation)
+  * [How to Use This Application](#How-to-use-this-application)
+  * [Contributors](#contributors)
+  * [Testing](#testing)
+  * [Deployed Application URL](#Deployed-application-url)
+  * [Questions](#questions)
+  
+  ## Description
+  A random password generator, follow the instructions and choose the lenght and  type of  characters to  recieve a random generated  password.
+  
+  ## Installation
+  Runs on the browser.
+  
+  ## How to Use This Application
+  Follow the instructions to meet your personal criteria.
+  
+  ## Contributors
+  Personal proyect
+  
+  ## Testing
+  N/A
+  
+  ## Deployed Application URL
+  undefined
+  
+
+  
+  ## Questions
+  Any questions contact me [here](mailto:adrianc.rm0@gmail.com?subject=[GitHub]%20Dev%20Connect) or contact [github/https://github.com/carlos-roma](https://github.com/https://github.com/carlos-roma).
+  
